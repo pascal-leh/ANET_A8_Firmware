153a4bedf01d542f5a45942f1c2e47ef90121038
Pascal <pascal.lehni@gmail.com>
2023-02-24 10:51:28 +0100

    edit version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Anet A8 v5"
+#define CUSTOM_MACHINE_NAME "Anet A8 v0.6  "
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
